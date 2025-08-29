fc9f245071da7194fa8b13a63b222207ef8fbe6e
Buruk Kassahun <brookkasahun@gmail.com>
2025-08-29 15:53:51 +0300

    Fixed user model

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -15,7 +15,7 @@ class RegisterView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        token, created = Token.objects.get_or_create(user=user)
+        token = Token.objects.get(user=user)
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": token.key
